8a1fb744aa4deab74c7f3b6a7855978ba155c549
yoshisuke1013 <>
2025-07-05 13:57:29 +0900

    コンテンツを共通コンポーネント化

--- src/constants/site-metadata.ts
+++ src/constants/site-metadata.ts
@@ -6,16 +6,57 @@ interface SiteMetadata {
   SOCIAL_MEDIA: {
     github: string;
   };
+  SITE_TOP: string;
+  SITE_ABOUT: string;
+  SITE_SKILL: string;
 }
 
-const siteAuthor = "Yoshisuke1013";
+const siteAuthor = "yoshisuke1013";
+const siteRepository = `https://github.com/${siteAuthor}/portfolio.git`;
+
+const siteTopContent = `# Clone the repository
+git clone ${siteRepository}
+
+# Install dependencies
+cd portfolio
+npm install
+
+# Start your development journey
+npm run dev`;
+
+const siteAboutContent = `# Occupation(職種)
+Front-End Developer(フロントエンドエンジニア)
+
+# Career Start(エンジニア歴)
+It started in 2010.(2010年〜)
+
+# Work History(職歴)
+Web Application Development(Webアプリケーション開発)
+Website Creation(Webサイト開発)
+`;
+
+const siteSkillContent = `# Programming Language(言語)
+HTML,CSS,JavaScript,TypeScript
+
+# Library(ライブラリ)
+React,Vue.js,jQuery
+
+# Framework(フレームワーク)
+Next.js,Nuxt.js,Astro
+
+# CMS(コンテントマネジメントシステム)
+WordPress
+`;
 
 export const siteMetadata: SiteMetadata = {
   SITE_AUTHOR: siteAuthor,
   SITE_NAME: `${siteAuthor}'s Portfolio`,
   SITE_DESC: `${siteAuthor}'のポートフォリオです。`,
-  SITE_REPOSITORY: "https://github.com/yoshisuke1013/portfolio.git",
+  SITE_REPOSITORY: siteRepository,
   SOCIAL_MEDIA: {
     github: `https://github.com/${siteAuthor}`,
   },
+  SITE_TOP: siteTopContent,
+  SITE_ABOUT: siteAboutContent,
+  SITE_SKILL: siteSkillContent,
 };
